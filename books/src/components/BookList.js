1a2101465ecf7b9a26727eab5a55d1751f194479
jordy bakker <9015306@student.zadkine.nl>
2023-12-04 13:19:39 +0100

    fliter function

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,36 +1,60 @@
-import { useState } from 'react';
-import React from 'react';
-import Book from './Book';
-import './BookList.css';
+import { useState } from "react";
+import React from "react";
+import Book from "./Book";
+import "./BookList.css";
+
+
 
 const Booklist = () => {
-  const [books, setBooks] = useState([
+const [searchInpute, setsearchInpute] = useState('')
 
+  const [books, setBooks] = useState([
     {
-      title: 'lang levende liefde',
-      author: 'menno stoop',
-      image: './images/foto.png',
-  },
-  {
-      title: 'de grijze jager',
-      author: 'menno stoop',
-      image: './images/foto.png',
-  },
-  {
-      title:
-       'het leven van een loser',
-      author: 'menno stoop',
-      image: './images/foto.png',
-  },
+      title: "lang levende liefde",
+      author: "menno stoop",
+      image: "./images/foto.png",
+    },
+    {
+      title: "de grijze jager",
+      author: "menno stoop",
+      image: "./images/foto.png",
+    },
+    {
+      title: "het leven van een loser",
+      author: "menno stoop",
+      image: "./images/foto.png",
+    },
   ]);
-    return ( 
-        <section className='container'>
-          {books.map((book) =>(
-            <Book title ={book.title} author={book.author} image={book.image}/>
-          ))
-          }
-        </section>
-     );
-}
+
+  const handleChange = (e) => {
+    // e.preventDefault();
+    setsearchInpute(e.target.value);
+
+    const filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(searchInpute.toLowerCase())
+    
+    );
+    setBooks (filteredBooks)
+  };
+
  
+
+  return (
+    <section className="container">
+      <div className="search">
+        <input  
+        type='text'
+        placeholder="zoek een title"
+        onChange={handleChange}
+        value={searchInpute}
+        name="search"
+        />
+      </div>
+      {books.map((book) => (
+        <Book title={book.title} author={book.author} image={book.image} />
+      ))}
+    </section>
+  );
+};
+
 export default Booklist;
